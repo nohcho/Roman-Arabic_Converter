7f962397ae1508b5ec680d6ac2c865c2539e6da5
Muhammad <tasuhanow@mail.ru>
2022-11-12 01:31:30 +0500

    a first convert  function is created

--- components/Converter.tsx
+++ components/Converter.tsx
@@ -5,6 +5,33 @@ type RomanType = {
   roman: string;
 };
 
+const ToRoman = (num: number) => {
+  let roman = '',
+    romanLetters = [
+      'M',
+      'CM',
+      'D',
+      'CD',
+      'C',
+      'XC',
+      'L',
+      'XL',
+      'X',
+      'IX',
+      'V',
+      'IV',
+      'I'
+    ],
+    listOfNumber = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  const length = romanLetters.length;
+  for (let i = 0; i < length; i++) {
+    while (num >= listOfNumber[i]) {
+      roman = roman + romanLetters[i];
+      num = num - listOfNumber[i];
+    }
+  }
+  return roman;
+};
 
 const Converter = () => {
   const state = {
